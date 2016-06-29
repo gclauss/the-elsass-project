63fbfbbb7352dd1fc6d33b2ae1002b8a148dd93c
gclauss <claussgeoffroy@gmail.com>
2016-06-29 20:11:41 +0200

    added /random-village route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,36 @@ var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
+
+function getRandom(array) {
+	var randomFloat = Math.random();
+	var randomInt = Math.floor(array.length*randomFloat);
+	try {
+		return array[randomInt];
+	} catch(e) {
+		console.log('math error');
+		return array[0];			
+	}
+}
+
+var villages = [
+	{name : "Truchtersheim", exists : true},
+	{name : "Schluchendorf", exists : false, author : 'Geoffroy'},
+	{name : "Hoplagheim", exists : false, author : 'Martin'},
+	{name : "Bredelagheim", exists : false, author : 'Martin'},
+	{name : "Manalawihr", exists : false, author : 'Martin'},
+	{name : "Untersheim-le-haut", exists : false, author : 'Martin'},
+	{name : "Schloukheim", exists : false, author : 'Martin'},
+	{name : "Nachparisheim", exists : false, author : 'Martin'},
+	{name : "Haltelawihr", exists : false, author : 'Martin'},
+	{name : "Bierbuchberg", exists : false, author : 'Martin'},
+	{name : "Flammenwiller", exists : false, author : 'Martin'}
+];
+
+server.get('/random-village', function() {
+	res.send(JSON.stringify(getRandom(villages)));
+});
+
 server.get(/\/?.*/, restify.serveStatic({
     directory: __dirname + '/../client',
     default : 'index.html'
