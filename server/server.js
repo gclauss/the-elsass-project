9b6cb6fc8dba1db91b258aea3c8dc8e1393667b5
gclauss <claussgeoffroy@gmail.com>
2016-06-29 19:51:22 +0200

    test ecstatic

--- server/server.js
+++ server/server.js
@@ -1,14 +1,7 @@
 var port = process.env.PORT || 5000;
 
-/*var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/../client')).listen(port, function(){
-    console.log('Server running on ' + port + '...');
-});*/
-
-
-
 var restify = require('restify');
+var ecstatic = require('ecstatic');
 
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
@@ -19,10 +12,8 @@ var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.get(/\/docs\/current\/?.*/, restify.serveStatic({
-  directory: './documentation/v1',
-  default: 'index.html'
-}));
+server.use(ecstatic({ root: __dirname + '../client' }));
+
 
 server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
