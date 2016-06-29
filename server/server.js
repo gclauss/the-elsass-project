7e237e76c90f29f8548702b585d2c8d49b4fd972
gclauss <claussgeoffroy@gmail.com>
2016-06-29 19:36:10 +0200

    without connect

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var port = process.env.PORT || 5000;
 
-var connect = require('connect');
+/*var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname + '/../client')).listen(port, function(){
     console.log('Server running on ' + port + '...');
-});
+});*/
 
 
 
