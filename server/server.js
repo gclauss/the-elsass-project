17fb516d04f8ec0165e58889a3a4aa03adb53772
gclauss <claussgeoffroy@gmail.com>
2016-06-29 19:27:53 +0200

    trying restify with connect

--- server/server.js
+++ server/server.js
@@ -5,3 +5,20 @@ var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname + '/../client')).listen(port, function(){
     console.log('Server running on ' + port + '...');
 });
+
+
+
+var restify = require('restify');
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(port, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
